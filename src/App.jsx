4eac697ad9fadd31b063e36d4d6aa7fe061bbb69
elhadj <elhadjamadoubah@gmail.com>
2026-04-08 15:32:59 -0700

    implemented the add task state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddTask() {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  }
+
   return (
     <>
       <h1>Todo-list</h1>
-      <input type="text" placeholder="Enter your Task" />
-      <button>Add to do:</button>
-      <p>Coding day with Mubark</p>
+      <input
+        type="text"
+        placeholder="Enter your Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTask}>Add to do</button>
+
+      <ul>
+        {tasks.map((t, i) => (
+          <li key={i}>{t}</li>
+        ))}
+      </ul>
     </>
   );
 }
