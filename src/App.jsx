54f79e05595c40da95b184570ff500b62038fabf
elhadj <elhadjamadoubah@gmail.com>
2026-04-07 22:13:39 -0700

    projecte initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Todo-list</h1>
+      <input type="text" placeholder="Enter your Task" />
+      <button>Add to do:</button>
+      <p>Coding day with Mubark</p>
+    </>
+  );
+}
+
+export default App;
